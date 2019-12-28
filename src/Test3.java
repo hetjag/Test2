62f6d97721b649e0905a90755954354724554364
hetjag <Hetaldhameliya13@gmail.com>
2019-12-28 19:09:32 +0000

    created empty project

---
+++ src/Test3.java
@@ -0,0 +1,20 @@
+public class Test3 {
+
+
+    // No return type - No parameters STATIC Method
+    public static void addition(int c, int r) {
+
+
+        System.out.print("Addition of " + c + " and " + r + " = ");
+        System.out.println(c + r);
+
+
+    }
+
+    //main method
+    public static void main(String[] args) {
+        addition(10, 20);
+    }
+
+
+}
